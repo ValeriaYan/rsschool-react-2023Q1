ed4e658609b91629df964f23cbd4b9e81e0fce68
Valeria <valeria.yanycheva@gmail.com>
2023-04-02 20:21:16 +0600

    refactor: rewrite the HomePage component using hooks

--- react-components/src/pages/HomePage.tsx
+++ react-components/src/pages/HomePage.tsx
@@ -1,58 +1,96 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { CardsList } from '../components/cardList/CardsList';
 import { Search } from '../components/search/Search';
+import { Card } from '../components/cardList/CardsList';
 
-type HomePageState = {
-  error: Error | null;
-  products: [];
-  isLoaded: boolean;
-};
+// type HomePageState = {
+//   error: Error | null;
+//   products: [];
+//   isLoaded: boolean;
+// };
 
-class HomePage extends React.Component<unknown, HomePageState> {
-  constructor(props: unknown) {
-    super(props);
-    this.state = {
-      error: null,
-      products: [],
-      isLoaded: false,
-    };
-  }
+// class HomePage extends React.Component<unknown, HomePageState> {
+//   constructor(props: unknown) {
+//     super(props);
+//     this.state = {
+//       error: null,
+//       products: [],
+//       isLoaded: false,
+//     };
+//   }
 
-  componentDidMount(): void {
+//   componentDidMount(): void {
+//     fetch('https://api.escuelajs.co/api/v1/products')
+//       .then((res) => res.json())
+//       .then(
+//         (result) => {
+//           this.setState({
+//             products: result,
+//             isLoaded: true,
+//           });
+//         },
+//         (error) => {
+//           this.setState({
+//             isLoaded: true,
+//             error: error,
+//           });
+//         }
+//       );
+//   }
+
+//   render(): React.ReactNode {
+//     const { isLoaded, error, products } = this.state;
+//     if (error) {
+//       return <div className="error">Error: {error.message}</div>;
+//     } else if (!isLoaded) {
+//       return <div className="loading">Loading...</div>;
+//     } else {
+//       return (
+//         <>
+//           <Search />
+//           <div className="card-list">
+//             <CardsList products={products} />
+//           </div>
+//         </>
+//       );
+//     }
+//   }
+// }
+
+function HomePage(): React.ReactNode {
+  const [error, setError] = useState<Error | null>(null);
+  const [products, setProducts] = useState<Card[]>([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
     fetch('https://api.escuelajs.co/api/v1/products')
       .then((res) => res.json())
       .then(
         (result) => {
-          this.setState({
-            products: result,
-            isLoaded: true,
-          });
+          setProducts(result);
+          setIsLoaded(true);
         },
         (error) => {
-          this.setState({
-            isLoaded: true,
-            error: error,
-          });
+          setIsLoaded(true);
+          setError(error);
         }
       );
-  }
+  }, []);
 
-  render(): React.ReactNode {
-    const { isLoaded, error, products } = this.state;
-    if (error) {
-      return <div className="error">Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div className="loading">Loading...</div>;
-    } else {
-      return (
-        <>
-          <Search />
-          <div className="card-list">
-            <CardsList products={products} />
-          </div>
-        </>
-      );
-    }
+  if (error) {
+    return <div className="error">Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div className="loading">Loading...</div>;
+  } else {
+    return (
+      <>
+        <Search />
+        {console.log('render')}
+        <div className="card-list">
+          <CardsList products={products} />
+        </div>
+      </>
+    );
   }
 }
 
