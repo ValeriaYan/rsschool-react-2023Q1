598101561d9fcf955a81b92c62dd10336cd977b1
Valeria <valeria.yanycheva@gmail.com>
2023-04-03 01:59:06 +0600

    refactor: rewrite all input components to functional components, rewrite UserForm component with using hooks

--- react-components/src/components/form/UserForm.tsx
+++ react-components/src/components/form/UserForm.tsx
@@ -1,175 +1,95 @@
 import React from 'react';
-import { DateInput } from './dateInput/DateInput';
-import { TextInput } from './textInput/TextInput';
-import { SelectInput } from './selectInput/SelectInput';
-import { CheckboxInput } from './checkboxInput/CheckboxInput';
-import { RadioInputs } from './radioInput/RadioInputs';
-import { FileInput } from './fileInput/FileInput';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import {
+  TextInput,
+  DateInput,
+  SelectInput,
+  FileInput,
+  RadioInputs,
+  CheckboxInput,
+} from './inputs/inputs';
 import { User } from '../userCardList/UsersList';
-
-type UserFormState = {
-  emptyNameField?: boolean;
-  emptyDateField?: boolean;
-  emptyConfirmCheckbox?: boolean;
-  emptyRadioFields?: boolean;
-  cardCreated: boolean;
-};
 
 type UserFormProps = {
   updateData: (users: User) => void;
 };
-class UserForm extends React.Component<UserFormProps, UserFormState> {
-  textRef: React.RefObject<HTMLInputElement>;
-  dateRef: React.RefObject<HTMLInputElement>;
-  selectRef: React.RefObject<HTMLSelectElement>;
-  checkboxRef: React.RefObject<HTMLInputElement>;
-  radioRefs: React.RefObject<HTMLInputElement>[];
-  fileRef: React.RefObject<HTMLInputElement>;
-  formRef: React.RefObject<HTMLFormElement>;
-  errors: {
-    emptyNameField: boolean;
-    emptyDateField: boolean;
-    emptyConfirmCheckbox: boolean;
-    emptyRadioFields: boolean;
-    emptySelectField: boolean;
-  };
-  constructor(props: UserFormProps) {
-    super(props);
-    this.textRef = React.createRef();
-    this.dateRef = React.createRef();
-    this.selectRef = React.createRef();
-    this.checkboxRef = React.createRef();
-    this.radioRefs = [React.createRef(), React.createRef()];
-    this.fileRef = React.createRef();
-    this.formRef = React.createRef();
 
-    this.state = {
-      cardCreated: false,
-    };
+export interface IFormValues {
+  name: string;
+  date: string;
+  country: string;
+  file: FileList;
+  gender: string;
+  confirm: boolean;
+}
 
-    this.errors = {
-      emptyNameField: false,
-      emptyDateField: false,
-      emptyConfirmCheckbox: false,
-      emptyRadioFields: false,
-      emptySelectField: false,
-    };
-    this.onSubmit = this.onSubmit.bind(this);
-  }
+function UserForm(props: UserFormProps) {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    getValues,
+    formState: { errors },
+  } = useForm<IFormValues>();
 
-  checkFields() {
-    if (this.textRef.current && this.textRef.current.value === '') {
-      this.errors['emptyNameField'] = true;
-    } else {
-      this.errors['emptyNameField'] = false;
-    }
-
-    if (this.dateRef.current && this.dateRef.current.value === '') {
-      this.errors['emptyDateField'] = true;
-    } else {
-      this.errors['emptyDateField'] = false;
-    }
-
-    const radiosNotEmpty = this.radioRefs.some((elem) => elem.current?.checked);
-    if (!radiosNotEmpty) {
-      this.errors['emptyRadioFields'] = true;
-    } else {
-      this.errors['emptyRadioFields'] = false;
-    }
-
-    if (this.checkboxRef.current && !this.checkboxRef.current.checked) {
-      this.errors['emptyConfirmCheckbox'] = true;
-    } else {
-      this.errors['emptyConfirmCheckbox'] = false;
-    }
-
-    if (this.selectRef.current && this.selectRef.current.value == 'default') {
-      this.errors['emptySelectField'] = true;
-    } else {
-      this.errors['emptySelectField'] = false;
-    }
-
-    console.log(this.selectRef?.current?.value);
-    return (
-      !this.errors['emptyNameField'] &&
-      !this.errors['emptyDateField'] &&
-      !this.errors['emptyRadioFields'] &&
-      !this.errors['emptyConfirmCheckbox']
-    );
-  }
-
-  onSubmit(event: React.FormEvent) {
-    event?.preventDefault();
-    if (this.checkFields()) {
-      let gender: string | undefined;
-      for (let i = 0; i < this.radioRefs.length; i++) {
-        if (this.radioRefs[i].current?.checked) {
-          gender = this.radioRefs[i].current?.value;
-        }
-      }
-      let photo = '';
-      if (this.fileRef.current?.files?.item(0)) {
-        photo = URL.createObjectURL(this.fileRef.current?.files?.item(0) as Blob);
-      }
-      this.props.updateData({
-        name: this.textRef.current?.value,
-        date: this.dateRef.current?.value,
-        gender: gender,
-        img: photo,
-        country: this.selectRef.current?.value,
-      });
-
-      this.setState({
-        cardCreated: true,
-      });
-      this.formRef.current?.reset();
-    }
-    this.setState({
-      cardCreated: false,
+  const onSubmit: SubmitHandler<IFormValues> = (data) => {
+    console.log(data);
+    props.updateData({
+      name: data.name,
+      date: data.date,
+      gender: data.gender,
+      img: URL.createObjectURL(data.file[0] as Blob),
+      country: data.country,
     });
-  }
+    reset();
+  };
 
-  componentDidUpdate() {
-    setTimeout(() => {
-      this.setState({
-        cardCreated: false,
-      });
-    }, 3000);
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.onSubmit} ref={this.formRef}>
-        {this.errors['emptyNameField'] && <div className="error">{'Name can not be empty'}</div>}
-        <TextInput textRef={this.textRef} labelText="Name: " />
-        {this.errors['emptyDateField'] && <div className="error">{'Date can not be empty'}</div>}
-        <DateInput dateRef={this.dateRef} labelText="Date of Birth: " />
-        {this.errors['emptySelectField'] && (
-          <div className="error">{'Country can not be empty'}</div>
-        )}
-        <SelectInput
-          selectRef={this.selectRef}
-          labelText="Choose country: "
-          values={['Russian', 'Ukraine', 'USA', 'Belarus', 'Poland', 'Kazakhstan']}
-        />
-        {this.errors['emptyRadioFields'] && (
-          <div className="error">{'Gender can not be empty'}</div>
-        )}
-        <div className="radio-buttons">
-          <RadioInputs radioRefs={this.radioRefs} name="gender" values={['man', 'woman']} />
-        </div>
-        <FileInput fileRef={this.fileRef} labelText="Choose user card image" />
-        {this.errors['emptyConfirmCheckbox'] && (
-          <div className="error">
-            {'You must confirm that you agree to the processing of personal data'}
-          </div>
-        )}
-        <CheckboxInput checkboxRef={this.checkboxRef} labelText="I consent to my personal data" />
-        <button type="submit">Create User Card</button>
-        {this.state.cardCreated && <div className="success-message">{'Card created'}</div>}
-      </form>
-    );
-  }
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <TextInput register={register} label="Name: " value="name" required="Name can't be empty!" />
+      <div className="error">{errors?.name && <p>{errors.name.message || 'Error!'}</p>}</div>
+      <DateInput
+        register={register}
+        label="Date of Birth: "
+        value="date"
+        required="Date can't be empty!"
+      />
+      <div className="error">{errors?.date && <p>{errors.date.message || 'Error!'}</p>}</div>
+      <SelectInput
+        label="Choose your country: "
+        value="country"
+        register={register}
+        values={['Russian', 'Ukraine', 'USA', 'Belarus', 'Poland', 'Kazakhstan']}
+        required="Country can't be empty!"
+      />
+      <div className="error">
+        {getValues('country') == 'default' && <p>{"Country can't be empty!"}</p>}
+      </div>
+      <RadioInputs
+        register={register}
+        name="gender"
+        value="gender"
+        values={['man', 'woman']}
+        required="Gender can't bу empty!"
+      />
+      <div className="error">{errors?.gender && <p>{errors.gender.message || 'Error!'}</p>}</div>
+      <FileInput
+        register={register}
+        label="Choose file"
+        value="file"
+        required="Please, choose file!"
+      />
+      <div className="error">{errors?.file && <p>{errors.file.message || 'Error!'}</p>}</div>
+      <CheckboxInput
+        register={register}
+        label="I consent to my personal data"
+        value="confirm"
+        required
+      />
+      <div className="error">{errors?.confirm && <p>{errors.confirm.message || 'Error!'}</p>}</div>
+      <button type="submit">Create User Card</button>
+    </form>
+  );
 }
 
 export { UserForm };
---
+++ react-components/src/components/form/inputs/inputs.tsx
@@ -0,0 +1,119 @@
+import React from 'react';
+import { Path, UseFormRegister } from 'react-hook-form';
+import { IFormValues } from '../UserForm';
+
+type TextInputProps = {
+  register: UseFormRegister<IFormValues>;
+  value: Path<IFormValues>;
+  label: string;
+  required: string | boolean;
+};
+
+type DateInputProps = {
+  register: UseFormRegister<IFormValues>;
+  value: Path<IFormValues>;
+  label: string;
+  required: string | boolean;
+};
+
+type SelectInputProps = {
+  register: UseFormRegister<IFormValues>;
+  value: Path<IFormValues>;
+  label: string;
+  values: string[];
+  required: string | boolean;
+};
+
+type FileInputProps = {
+  register: UseFormRegister<IFormValues>;
+  value: Path<IFormValues>;
+  label: string;
+  required: string | boolean;
+};
+
+type RadioInputsProps = {
+  register: UseFormRegister<IFormValues>;
+  value: Path<IFormValues>;
+  name: string;
+  values: string[];
+  required: string | boolean;
+};
+
+type CheckboxInputProps = {
+  register: UseFormRegister<IFormValues>;
+  value: Path<IFormValues>;
+  label: string;
+  required: string | boolean;
+};
+
+function TextInput({ label, value, register, required }: TextInputProps) {
+  return (
+    <label>
+      {label}
+      <input type="text" {...register(value, { required })} />
+    </label>
+  );
+}
+
+function DateInput({ label, value, register, required }: DateInputProps) {
+  return (
+    <label>
+      {label}
+      <input type="date" {...register(value, { required })} />
+    </label>
+  );
+}
+
+function SelectInput({ label, value, register, required, values }: SelectInputProps) {
+  return (
+    <label>
+      {label}
+      <select {...register(value, { required })}>
+        <option value="default">{'--choose--'}</option>
+        {values.map((item, index) => {
+          return (
+            <option value={item} key={index}>
+              {item}
+            </option>
+          );
+        })}
+      </select>
+    </label>
+  );
+}
+
+function FileInput({ label, value, register, required }: FileInputProps) {
+  return (
+    <label>
+      <span className="file-button">Choose file</span>
+      {label}
+      <input type="file" {...register(value, { required })} />
+    </label>
+  );
+}
+
+function RadioInputs({ name, value, values, register, required }: RadioInputsProps) {
+  return (
+    <div className="radio-buttons">
+      {values.map((elem, index) => {
+        return (
+          <label key={index}>
+            <input type="radio" name={name} value={elem} {...register(value, { required })} />
+            {elem}
+          </label>
+        );
+      })}
+    </div>
+  );
+}
+
+function CheckboxInput({ label, value, register, required }: CheckboxInputProps) {
+  return (
+    <label>
+      {label}
+      <input type="checkbox" {...register(value, { required })} />
+    </label>
+  );
+}
+
+export { TextInput, DateInput, SelectInput, FileInput, RadioInputs, CheckboxInput };
